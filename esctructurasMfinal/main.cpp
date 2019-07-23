4fd5f55df5f0e0baaac50f0e89d77db824f9f84f
RolyEsco <52805966+RolyEsco@users.noreply.github.com>
2019-07-23 08:37:32 -0400

    commit ultimo
    commit

--- esctructurasMfinal/main.cpp
+++ esctructurasMfinal/main.cpp
@@ -67,17 +67,46 @@ void mostrar()
     cout<<endl<<"--------------------------------"<<endl;
 }
 
-void mostrar2(){
-    cout<<endl<<"** INFORMACION DE LA ESTRUCTURA **"<<endl;
-    for(int k=0; k<N ; k++)
-    {   cout << "-- Registro No. "<<k+1<< " --"<<endl;
+
+void mostrar_registro(int k)
+{
+        cout << "-- Registro No. "<<k+1<< " --"<<endl;
         printf("\n\t NOMBRE   : %-20s", programacionII[k].nombre);
         printf("\n\t APELLIDO : %-30s", programacionII[k].apellidos);
         printf("\n\t CELULAR  : %-15s", programacionII[k].celular);
         printf("\n\t EDAD     : %5d", programacionII[k].edad);
-    }
 }
 
+void mostrar2(){
+    cout<<endl<<"** INFORMACION DE LA ESTRUCTURA **"<<endl;
+    for(int k=0; k<N ; k++)
+    {   mostrar_registro(k);
+            }
+}
+
+void modificar()
+{   cout<<endl<<"** MODIFICAR REGISTRO **"<<endl;
+    int x;
+    printf("\n\nIngrese la posicion a modificar [1, %d]",N);
+    cin>>x;
+    x--;
+    if(x>=0 && x<N)
+    {   mostrar_registro(x);
+        cout <<endl<<"-- Registro No. "<<x+1<< " --"<<endl;
+        cout << "Nombre :";
+        cin>>programacionII[x].nombre;
+        cout << "Apellidos :";
+        cin>>programacionII[x].apellidos;
+        cout << "Celular :";
+        cin>>programacionII[x].celular;
+        cout << "Edad :";
+        cin>>programacionII[x].edad;
+    }
+    else
+    cout<<endl<<"El subindice ingresado no es valido";
+
+
+}
 void ordenar()
 {   alumno auxiliar;
     for(int i=0; i<N-1; i++)
@@ -102,7 +131,8 @@ int main()
         cout << " (2) Ingresar N registros" << endl;
         cout << " (3) Mostrar datos" << endl;
         cout << " (4) Ordenar por nombre" << endl;
-        cout << " (5) Calcular edad promedio" << endl;
+        cout << " (5) Modificar registro" << endl;
+        cout << " (6) Calcular edad promedio" << endl;
         cout << " (ESC) para salir" << endl;
         opcion=getch();
         switch(opcion)
@@ -110,7 +140,8 @@ int main()
             case '2': ingresar_N();  break;
             case '3': mostrar();  break;
             case '4': ordenar();  break;
-            case '5': edad_promedio();  break;
+            case '5': modificar();  break;
+            case '6': edad_promedio();  break;
         }
 
     }while(opcion!=27);
