aa28857c4ee2bda6a23528360dc98f5890341c70
RolyEsco <52805966+RolyEsco@users.noreply.github.com>
2019-07-24 09:05:58 -0400

    commit
    commit

--- archivo1/main.cpp
+++ archivo1/main.cpp
@@ -1,19 +1,53 @@
 #include <iostream>
 #include <stdio.h>
+#include <string.h>
 
 using namespace std;
 
 int main()
 {
 
-    FILE *archivo;
+    FILE *archivo, *origen, *destino;
+    char nom_archivo[20];
+    //strcpy(src,  "archivo");
+    string cadena;
+    int x;
 
-    archivo=fopen("hola_mundo.txt","w+");
+    destino=fopen("union.txt","a+");
+    origen=fopen("numero1.txt","r");
 
-    fprintf(archivo,"HOLA MUNDO");
+    while(fscanf(origen,"%d",&x)!=EOF)
+    {
+        printf("\n%4d",x);
+        fprintf(destino,"\n%4d",x);
 
-    fclose(archivo);
+    }
 
-    cout << "Hello world!" << endl;
+    fclose(origen);
+
+    origen=fopen("numero2.txt","r");
+    while(fscanf(origen,"%d",&x)!=EOF)
+    {
+        printf("\n%4d",x);
+        fprintf(destino,"\n%4d",x);
+
+    }
+    fclose(origen);
+
+    fclose(destino);
+
+/*
+    for(int i=0; i<10; i++)
+        {
+            //cadena="archivo"+i+".txt";
+            //nom_archivo=strcat("archivo", "20.txt");
+            //nom_archivo=strcat(nom_archivo,".txt");
+            archivo=fopen("continuar.txt","w+");
+
+            fprintf(archivo,"%d",i+1);
+
+            fclose(archivo);
+        }
+    cout << "Hello world!" << endl;*/
     return 0;
 }
